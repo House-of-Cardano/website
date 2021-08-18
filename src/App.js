c04003c399d119c73c8079c5f098531ad8b7a466
simonholmes001 <simonholmesabc@gmail.com>
2021-08-18 16:14:20 +0200

    s

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './HoC_2.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="Landing-Page-Header">
+          House of Cardano
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
